254fe440abbb4e231e4d2d344751b96415063f56
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-01-28 23:05:20 +0100

    [GSM] "view ophonekitd log" function

--- shr_settings_modules/shr_gsm.py
+++ shr_settings_modules/shr_gsm.py
@@ -90,9 +90,75 @@ class Gsm(module.AbstractModule):
     def goto_settingsbtClick(self, obj, event, *args, **kargs):
         self.wininfo.hide()
         self.winope.hide()
+
+    def destroyDebug(self, obj, event, *args, **kargs):
+        self.windeb.hide()
+
+    def clean_ophonekitd_log(self, obj, event, *args, **kargs):
+        os.system("echo \"\" >  /var/log/ophonekitd.log")
+        self.destroyDebug(self, obj, event, *args, **kargs) 
+       
+    def view_ophonekitd_log(self, obj, event, *args, **kargs):        
+        print "ophonekitd log"
+        self.windeb = elementary.Window("ophonekitdLog", elementary.ELM_WIN_BASIC )
+        self.windeb.title_set("ophonekitd log")
+        self.windeb.autodel_set(True)
+        self.bgdeb = elementary.Background(self.windeb)
+        self.windeb.resize_object_add(self.bgdeb)
+        self.bgdeb.size_hint_weight_set(1.0, 1.0)
+        self.bgdeb.show()
+
+        box0 = elementary.Box(self.windeb)
+        box0.size_hint_weight_set(1.0, 1.0)
+        self.windeb.resize_object_add(box0)
+        box0.show()
+
+        fr = elementary.Frame(self.windeb)
+        fr.label_set("ophonekitd log")
+        fr.size_hint_align_set(-1.0, 0.0)
+        box0.pack_end(fr)
+        fr.show()
+
+        sc = elementary.Scroller(self.windeb)
+        sc.size_hint_weight_set(1.0, 1.0)
+        sc.size_hint_align_set(-1.0, -1.0)
+        box0.pack_end(sc)
+        sc.show()
+
+        cleanbt = elementary.Button(self.windeb)
+        cleanbt.clicked = self.clean_ophonekitd_log   
+        cleanbt.label_set("Clean")
+        cleanbt.size_hint_align_set(-1.0, 0.0)
+        cleanbt.show()
+        box0.pack_end(cleanbt)
+
+        cancelbt = elementary.Button(self.windeb)
+        cancelbt.clicked = self.destroyDebug
+        cancelbt.label_set("Close")
+        cancelbt.size_hint_align_set(-1.0, 0.0)
+        cancelbt.show()
+        box0.pack_end(cancelbt)
+
+        box1 = elementary.Box(self.windeb)
+        box1.size_hint_weight_set(1.0, -1.0)
+        sc.content_set(box1)
+        box1.show()
+
+        self.windeb.show()
         
-        
-    
+
+        c = open( "/var/log/ophonekitd.log", "r" )
+        while 1:
+            line = c.readline()
+            if not line:
+                break
+            print "line ["+line+"]"
+            lb = elementary.Label(self.windeb)
+            lb.label_set(line)
+            box1.pack_end(lb)
+            lb.show()
+
+
     def operatorSelect(self, obj, event, *args, **kargs):
         #os.popen("echo \"gsmnetwork.RegisterWithProvider( "+obj.get_opeNr()+" )\" | cli-framework", "r");
         print "GSM operatorSelect [info] ["+str(obj.get_opeNr())+"]"
@@ -271,7 +337,7 @@ class Gsm(module.AbstractModule):
 
     def start_ophonekitd_btClick(self, obj, event, *args, **kargs):
         obj.hide()
-        os.system( "DISPLAY=:0 ophonekitd &" )
+        os.system( "DISPLAY=:0 ophonekitd >> /var/log/ophonekitd.log 2>&1 &" )
 
 
 
@@ -330,10 +396,18 @@ class Gsm(module.AbstractModule):
 
                 startbt = elementary.Button(self.window)
                 startbt.clicked = self.start_ophonekitd_btClick
-                startbt.label_set("yes")
+                startbt.label_set("Yes")
                 startbt.size_hint_align_set(-1.0, 0.0)
                 startbt.show()
                 boxOp.pack_end(startbt)
+
+                logbt = elementary.Button(self.window)
+                logbt.clicked = self.view_ophonekitd_log
+                logbt.label_set("View log")
+                logbt.size_hint_align_set(-1.0, 0.0)
+                logbt.show()
+                boxOp.pack_end(logbt)
+
 
                 fo = elementary.Frame(self.window)
                 fo.label_set( "ophonekitd" )
