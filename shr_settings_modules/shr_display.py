053dcab497bd8af30191c392b1a05e308b97c320
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-03-23 22:20:11 +0100

    [display] introduce Display module with backlight setting

---
+++ shr_settings_modules/shr_display.py
@@ -0,0 +1,57 @@
+import elementary
+import module
+import dbus
+import ecore
+
+# Locale support
+import gettext
+
+try:
+    cat = gettext.Catalog("shr-settings")
+    _ = cat.gettext
+except IOError:
+    _ = lambda x: x
+
+
+def getDbusObject (bus, busname , objectpath , interface):
+        dbusObject = bus.get_object(busname, objectpath)
+        return dbus.Interface(dbusObject, dbus_interface=interface)
+
+def handler():
+    return 0
+
+def error(msg):
+    return 0
+
+class Display(module.AbstractModule):
+    name = _("Display")
+
+    def setbacklight(self):
+        if self.value != self.slider.value:
+            self.display.SetBrightness(self.slider.value, reply_handler=handler, error_handler=error)
+            self.value = self.slider.value
+        return 1
+
+    def getbacklight(self):
+        return self.display.GetBrightness()
+    
+    def isEnabled(self):
+        try:
+            self.display = getDbusObject (self.dbus, "org.freesmartphone.odeviced", "/org/freesmartphone/Device/Display/0", "org.freesmartphone.Device.Display")
+            return 1
+        except:
+            return 0
+
+    def createView(self):
+        self.slider = elementary.Slider(self.window)
+        self.slider.label_set(_("Backlight "))
+        #slider.span_size_set(160)
+        #slider.size_hint_align_set(0, 0.5)
+        #slider.size_hint_weight_set(0, 1)
+        self.slider.unit_format_set(" %3.0f%% ")
+        self.slider.min_max_set(0, 100)
+        self.value = self.getbacklight()
+        self.slider.value = self.value
+        self.slider.show()
+        ecore.timer_add(1, self.setbacklight)
+        return self.slider
