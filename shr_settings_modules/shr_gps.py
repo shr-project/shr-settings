56a221a3a884500ec60348411207917d069df546
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-04-15 18:49:37 +0200

    [gps] (Michael Scherer) [PATCH 1/2] add a title to the satelite detail windows

--- shr_settings_modules/shr_gps.py
+++ shr_settings_modules/shr_gps.py
@@ -20,6 +20,7 @@ class SatDetails():
     def __init__(self, parent):
        self.satlab = [] #contains the satellite labels
        self.win = elementary.Window("Satellite details", elementary.ELM_WIN_DIALOG_BASIC)
+       self.win.title_set(_("Satellite details"))
        self.win.show()
        self.win.destroy = self.destroy
 
