e8834ef9180063f84615ce6795017deeb594128a
Cameron Frazier <frazier.cameron@gmail.com>
2009-04-18 22:10:33 -0400

    Added current charging rate to Status

--- shr_settings_modules/shr_battery.py
+++ shr_settings_modules/shr_battery.py
@@ -32,6 +32,7 @@ class Battery(module.AbstractModule):
         cur = "1234"
         cap = "1234"
         time = "1234"
+        rate = ""
 
         try:
             vol =  os.popen("cat /sys/class/power_sup*ly/bat*/voltage_now").readline().replace("\n","")
@@ -41,13 +42,15 @@ class Battery(module.AbstractModule):
             cap = os.popen("cat /sys/class/power_su*ply/bat*/capacity").readline().replace("\n","")
             if sta=="Charging":
                 time = os.popen("cat /sys/class/power_su*ply/bat*/time_to_full_now").readline().replace("\n","")
+                rate = os.popen("cat /sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/usb_curlim").readline().replace("\n","")
+                rate = " (%smA)" % rate
             else:
                 time = os.popen("cat /sys/class/power_su*ply/bat*/time_to_empty_now").readline().replace("\n","")
 
             self.voll.label_set(_("Voltage: ")+str(vol)[0]+"."+str(vol)[1]+str(vol)[2]+str(vol)[3]+" V")
             self.templ.label_set(_("Temperature: ")+str(temp)[0]+str(temp)[1]+"."+str(temp)[2]+" 'C")
             self.curl.label_set(_("Current: ")+str(cur)+" mA")
-            self.stal.label_set(_("Status: ")+sta)
+            self.stal.label_set(_("Status: ")+sta+rate)
             self.capl.label_set(_("Capacity: ")+cap+" %")
             self.timel.label_set(_("Remaining time: ")+self.sectotime(int(time)))
 
