af900fbd005f31ce12458dcef3fe8ec04d21be60
Cameron Frazier <frazier.cameron@gmail.com>
2009-04-21 21:44:52 -0400

    Overall cleanup to improve clarity

--- shr_settings_modules/shr_device_timeouts.py
+++ shr_settings_modules/shr_device_timeouts.py
@@ -12,6 +12,9 @@ try:
 except IOError:
     _ = lambda x: x
 
+def getDbusObject (bus, busname , objectpath , interface):
+        dbusObject = bus.get_object(busname, objectpath)
+        return dbus.Interface(dbusObject, dbus_interface=interface)
 
 class ValueLabel( elementary.Label ):
     """ Label that displays current timeout """
@@ -26,171 +29,139 @@ class ValueLabel( elementary.Label ):
          self.label_set(str(val))
          self._value = val
 
+class IncDecButton(elementary.Button):
+    """
+    Button that add/substracts from the value label
+    """
 
-class AddButton( elementary.Button ):
-    """ Button that add/substracts from the value label """
-    def set_extData( self, addStep, name, labelObj):
-        self._addStep = addStep
-        self.name = name
-        self._labelObj = labelObj
+    def set_Delta(self, delta):
+        self._delta = delta
+        self.label_set("{0:+d}".format(delta))
 
-    def get_addStep( self ):
-        return self._addStep
+    def get_Delta( self ):
+        return self._delta
 
-    def get_name(self):
-        return self.name
 
-    def get_labelObj(self):
-        return self._labelObj
+class IncDecButtonBox(elementary.Box):
+    """
+    Object which shows an increment/decrement button set to alter int
+    Preferences values
+    """
+    
+    def IncDecButtonClick(self, obj, event, *args, **kargs):
+        """ 
+        Callback function when +-[1,10] timeout buttons have been pressed 
+        """
+        cur_val = self.cur_value
+        delta  = obj.get_Delta()
+        new_val = max(-1, cur_val + delta)
 
+        self.dbusObj.SetTimeout(self.item_name,int(new_val))
+        self.update()
+
+    def update(self):
+        """
+        Updates the displayed value to the current profile
+        """
+        timeouts = self.dbusObj.GetTimeouts()
+        self.cur_value = timeouts[self.item_name]
+        self.itemValue.set_value(self.cur_value) #implicitely sets label too
+
+    def setup(self):
+        """
+        Function to show a increment/decrement button set to alter int
+        Preferences values
+
+        Layout developed from shr_device_timeouts.py
+        """
+
+        self.horizontal_set(True)
+        self.size_hint_align_set(-1.0, 0.0)
+        self.size_hint_weight_set(1.0, 0.0)
+
+        timeouts = self.dbusObj.GetTimeouts()
+        self.cur_value = timeouts[self.item_name]
+
+        itemLabel = elementary.Label(self.window)
+        itemLabel.size_hint_weight_set(1.0, 0.0)
+        itemLabel.label_set(self.item_name)
+        itemLabel.show()
+
+        itemFrame = elementary.Frame(self.window)
+        itemFrame.style_set("outdent_top")
+        itemFrame.content_set(itemLabel)
+        itemFrame.show()
+
+        self.itemValue = ValueLabel(self.window)
+        self.itemValue.size_hint_weight_set(1.0, 0.0)
+        self.itemValue.set_value(self.cur_value) #implicitely sets label too
+        self.itemValue.show()
+
+        boxbox = elementary.Box(self.window)
+        boxbox.pack_start(itemFrame)
+        boxbox.pack_end(self.itemValue)
+        boxbox.size_hint_weight_set(1.0, 0.0)
+        boxbox.show()
+
+        buttons = []
+
+        for step in [-10, -1, 1, 10]:
+            btn = IncDecButton(self.window)
+            btn.set_Delta( step )
+            btn.clicked = self.IncDecButtonClick
+            btn.size_hint_align_set(-1.0, 0.0)
+            btn.show()
+
+            buttons.append(btn)
+
+        self.pack_end(buttons[0])
+        self.pack_end(buttons[1])
+        self.pack_end(boxbox)
+        self.pack_end(buttons[2])
+        self.pack_end(buttons[3])
+
+        self.show()
+
+    def __init__(self, win, dbusObj, item_name):
+        """
+        initialize the box and load objects
+        """
+        super(IncDecButtonBox, self).__init__(win)
+        self.window = win
+        self.dbusObj = dbusObj
+        self.item_name = item_name
+
+        self.setup()
 
 class Timeouts(module.AbstractModule):
     name = _("Timeouts")
 
+    def error(self):
+        label = elementary.Label(self.window)
+        label.label_set("Dbus is borked")
+        self.main.pack_start(label)
 
-    #----------------------------------------------------------------------------#
-    def getObject( self, busname, objectpath ):
-    #----------------------------------------------------------------------------#
-        #DBusGMainLoop(set_as_default=True)
-        #bus = dbus.SystemBus()
-        return self.dbus.get_object( busname, objectpath, follow_name_owner_changes=True )
-
-    #----------------------------------------------------------------------------#
-    def getInterface( self, busname, objectpath, interface ):
-    #----------------------------------------------------------------------------#
-        proxy = self.getObject( busname, objectpath )
-        return dbus.Interface( proxy, interface)
-
-    #----------------------------------------------------------------------------#
-    def getObjectsForInterface( self, interface ):
-    #----------------------------------------------------------------------------#
-        framework = getObject( "org.freesmartphone.frameworkd", "/org/freesmartphone/Framework" )
-
-        paths = framework.ListObjectsByInterface( interface, dbus_interface="org.freesmartphone.Framework" )
-        result = {}
-        for path in paths:
-            result[str(path)] = self.getInterface( "org.freesmartphone.frameworkd", path, interface )
-        return result
-
-
-    #----------------------------------------------------------------------------#   
-    def addbtClick(self, obj, event, *args, **kargs):
-    #----------------------------------------------------------------------------#
-        """ Callback function when +-[1,10] timeout buttons have been pressed """
-        label   = obj.get_labelObj()
-        cur_val = label.get_value()
-        addFor  = obj.get_addStep()
-        name    = obj.get_name()
-        new_val = max(-1, cur_val + addFor)
-
-        if self.dbus_state == 1:
-            self.devidle.SetTimeout( name, new_val )
-            label.set_value(new_val)
-
-
-    
-    #----------------------------------------------------------------------------#   
-    def createView(self):
-    #----------------------------------------------------------------------------#   
-        self.box1 = elementary.Box(self.window)
-
-
-        boxOp = elementary.Box(self.window)
-        boxOp.size_hint_weight_set(1.0, 1.0)
-        boxOp.size_hint_align_set(-1.0, 0.0)
-
+    def createView(self): 
+        self.main = elementary.Box(self.window)
 
         self.dbus_state = 0
         try:
-            self.devidle = self.getInterface( \
+            self.dbusObj = getDbusObject( self.dbus,
                 "org.freesmartphone.odeviced",
                 "/org/freesmartphone/Device/IdleNotifier/0",
-                "org.freesmartphone.Device.IdleNotifier" \
-                )
-            self.timeouts = self.devidle.GetTimeouts()
+                "org.freesmartphone.Device.IdleNotifier")
+            self.timeouts = self.dbusObj.GetTimeouts()
             self.dbus_state = 1
         except:
             self.dbus_state = 0
+            self.error()
 
-        if self.dbus_state == 0:
-            stal = elementary.Label(self.window)
-            stal.size_hint_align_set(-1.0, 0.0)
-            stal.label_set(_("can't connect to dbus"))
-            stal.show()
-            self.box1.pack_start(stal)
-        else:
-            # create a table that display states, values and buttons
-            tout_table = elementary.Table(self.box1)
-            tout_table.size_hint_align_set(-1.0, 0.5)
-
-            row = 0
+        if self.dbus_state:
             for i in self.timeouts:
                 if not str(i) in ("awake","busy","none"):
-                    row += 1
-                    namel = elementary.Label(self.window)
-                    namel.size_hint_align_set(-1.0, 0.5)
-                    namel.size_hint_weight_set(1.0, 1.0)
-                    namel.label_set(" "+str(i).replace("_"," ")+" ")
-                    namel.show()
-                    #tout_table.pack(namel,1,row,1,1)
+                    box = IncDecButtonBox(self.window, self.dbusObj, i)
+                    box.show()
+                    self.main.pack_end(box)
+            self.main.show()
 
-                    cur_val = int(self.timeouts[i])
-                    valuel  = ValueLabel(self.window)
-                    valuel.size_hint_align_set(0.5, 0.5)
-                    valuel.label_set(str(cur_val))
-                    valuel.set_value(cur_val) #implicitely sets label too
-                    valuel.show()
-
-                    framel = elementary.Frame(self.window)
-                    framel.style_set("outdent_top")
-                    framel.content_set(namel)
-                    framel.show()
-
-                    boxbox = elementary.Box(self.window)
-                    boxbox.pack_start(framel)
-                    boxbox.pack_end(valuel)
-                    boxbox.show()
-                    tout_table.pack(boxbox,3,row,1,1)
-
-                    # the -10 button
-                    mintenbt = AddButton(self.window)
-                    mintenbt.set_extData( -10, str(i), valuel)
-                    mintenbt.clicked = self.addbtClick
-                    mintenbt.label_set("-10")
-                    mintenbt.size_hint_align_set(-1.0, 0.0)
-                    mintenbt.show()
-                    tout_table.pack(mintenbt,1,row,1,1)
-
-                    # the -1 button
-                    minonebt = AddButton(self.window)
-                    minonebt.set_extData( -1, str(i), valuel) 
-                    minonebt.clicked = self.addbtClick
-                    minonebt.label_set("-1")
-                    minonebt.size_hint_align_set(-1.0, 0.0)
-                    minonebt.show()
-                    tout_table.pack(minonebt,2,row,1,1)
-
-                    # the +1 button
-                    addonebt = AddButton(self.window)
-                    addonebt.set_extData( 1, str(i), valuel)
-                    addonebt.clicked = self.addbtClick
-                    addonebt.label_set("+1")
-                    addonebt.size_hint_align_set(-1.0, 0.0)
-                    addonebt.show()
-                    tout_table.pack(addonebt,4,row,1,1)
-
-
-                    # the +10 button
-                    addtenbt = AddButton(self.window)
-                    addtenbt.set_extData( 10, str(i), valuel) 
-                    addtenbt.clicked = self.addbtClick
-                    addtenbt.label_set("+10")
-                    addtenbt.size_hint_align_set(-1.0, 0.0)
-                    addtenbt.show()
-                    tout_table.pack(addtenbt,5,row,1,1)
-
-            # finally show the table
-            tout_table.show()
-            self.box1.pack_start(tout_table)
-        
-        return self.box1
+        return self.main
