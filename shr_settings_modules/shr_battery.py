f9a056b0be272bbd4d1d4f03e9888535d1e1a656
Cameron Frazier <frazier.cameron@gmail.com>
2009-04-21 21:43:44 -0400

    General clean up
    Added current charge rate to status label, in anticipation of 'force-fast-charging' option being added

--- shr_settings_modules/shr_battery.py
+++ shr_settings_modules/shr_battery.py
@@ -12,6 +12,17 @@ except IOError:
     _ = lambda x: x
 
 
+class BatteryLabel(elementary.Label):
+    """
+    Generic class for the labels used below
+    """
+
+    def __init__(self,win):
+        super(BatteryLabel, self).__init__(win)
+        self.size_hint_align_set(-1.0, 0.0)
+        self.show()
+
+
 class Battery(module.AbstractModule):
     name = _("Battery")
 
@@ -35,80 +46,69 @@ class Battery(module.AbstractModule):
         rate = ""
 
         try:
-            vol =  os.popen("cat /sys/class/power_sup*ly/bat*/voltage_now").readline().replace("\n","")
-            temp = os.popen("cat /sys/class/power_sup*ly/bat*/temp").readline().replace("\n","")
-            cur =  int(os.popen("cat /sys/class/power_su*ply/bat*/current_now").readline().replace("\n",""))/1000
-            sta = os.popen("cat /sys/class/power_su*ply/bat*/status").readline().replace("\n","")
-            cap = os.popen("cat /sys/class/power_su*ply/bat*/capacity").readline().replace("\n","")
+            vol =  float(os.popen("cat /sys/class/power_sup*ly/bat*/voltage_now").readline())/(1000*1000)
+            temp = float(os.popen("cat /sys/class/power_sup*ly/bat*/temp").readline())/10
+            cur =  int(os.popen("cat /sys/class/power_su*ply/bat*/current_now").readline())/1000
+            sta = os.popen("cat /sys/class/power_su*ply/bat*/status").readline().strip()
+            cap = os.popen("cat /sys/class/power_su*ply/bat*/capacity").readline().strip()
             if sta=="Charging":
-                time = os.popen("cat /sys/class/power_su*ply/bat*/time_to_full_now").readline().replace("\n","")
-                rate = os.popen("cat /sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/usb_curlim").readline().replace("\n","")
-                rate = " (%smA)" % rate
+                time = int(os.popen("cat /sys/class/power_su*ply/bat*/time_to_full_now").readline())
+                try:
+                    rate = int(os.popen("cat /sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/usb_curlim").readline())
+                    rate = " (%s mA)" % rate
+                except:
+                    pass
             else:
-                time = os.popen("cat /sys/class/power_su*ply/bat*/time_to_empty_now").readline().replace("\n","")
+                time = int(os.popen("cat /sys/class/power_su*ply/bat*/time_to_empty_now").readline())
 
-            self.voll.label_set(_("Voltage: ")+str(vol)[0]+"."+str(vol)[1]+str(vol)[2]+str(vol)[3]+" V")
-            self.templ.label_set(_("Temperature: ")+str(temp)[0]+str(temp)[1]+"."+str(temp)[2]+" 'C")
-            self.curl.label_set(_("Current: ")+str(cur)+" mA")
-            self.stal.label_set(_("Status: ")+sta+rate)
-            self.capl.label_set(_("Capacity: ")+cap+" %")
-            self.timel.label_set(_("Remaining time: ")+self.sectotime(int(time)))
+            self.voll.label_set("{0}{1:.3f} V".format(_("Voltage: "),vol))
+            self.templ.label_set("{0}{1:.1f} 'C".format(_("Temperature: "),temp))
+            self.curl.label_set("{0}{1} mA".format(_("Current: "),str(cur)))
+            self.stal.label_set("{0}{1}".format(_("Status: "),sta+rate))
+            self.capl.label_set("{0}{1} %".format(_("Capacity: "),cap))
+            self.timel.label_set("{0}{1}".format(_("Remaining time: "),self.sectotime(int(time))))
 
 
             #FIXME: if it does not work.. we should try again?
+            #   Is there no way to use dbus? 
+            #   Timers seem to lock up the GUI
+            #               - Cameron
             if self.guiUpdate:
-                ecore.timer_add( 2.3, self.refreshAct)
+                ecore.timer_add( 5, self.refreshAct)
 
         except:
             print ":("
 
     def createView(self):
+
         self.guiUpdate = 1
-        self.box1 = elementary.Box(self.window)
 
+        # create the box
+        self.box = elementary.Box(self.window)
+        self.box.size_hint_weight_set(1.0, 1.0)
+        self.box.size_hint_align_set(-1.0, 0.0)
 
-        box1p = elementary.Box(self.window)
-        box1p.size_hint_weight_set(1.0, 1.0)
-        box1p.size_hint_align_set(-1.0, 0.0)
+        # create labels
+        self.stal   = BatteryLabel(self.window)
+        self.voll   = BatteryLabel(self.window)
+        self.templ  = BatteryLabel(self.window)
+        self.curl   = BatteryLabel(self.window)
+        self.capl   = BatteryLabel(self.window)
+        self.timel  = BatteryLabel(self.window)
 
-        self.stal = elementary.Label(self.window)
-    	self.stal.size_hint_align_set(-1.0, 0.0)
-    	self.stal.show()
-    	box1p.pack_start(self.stal)
+        # pack labels into the box
+        self.box.pack_start(self.stal)
+        self.box.pack_start(self.voll)
+        self.box.pack_start(self.templ)
+        self.box.pack_start(self.curl)
+        self.box.pack_start(self.capl)
+        self.box.pack_start(self.timel)
 
-        self.voll = elementary.Label(self.window)
-    	self.voll.size_hint_align_set(-1.0, 0.0)
-    	self.voll.show()
-    	box1p.pack_start(self.voll)
-
-        self.templ = elementary.Label(self.window)
-    	self.templ.size_hint_align_set(-1.0, 0.0)
-    	self.templ.show()
-    	box1p.pack_start(self.templ)
-
-        self.curl = elementary.Label(self.window)
-    	self.curl.size_hint_align_set(-1.0, 0.0)
-    	self.curl.show()
-    	box1p.pack_start(self.curl)
-
-        self.capl = elementary.Label(self.window)
-    	self.capl.size_hint_align_set(-1.0, 0.0)
-    	self.capl.show()
-    	box1p.pack_start(self.capl)
-
-        self.timel = elementary.Label(self.window)
-        self.timel.size_hint_align_set(-1.0, 0.0)
-        self.timel.show()
-        box1p.pack_start(self.timel)
-
-
-        box1p.show()
-        self.box1.pack_end(box1p)
-
+        self.box.show()
 
         self.refreshAct()
 
-        return self.box1
+        return self.box
 
     def stopUpdate(self):
         print "Bat desktructor"
