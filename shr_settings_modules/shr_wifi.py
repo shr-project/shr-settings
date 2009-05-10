56bedc17a1b543f095ed8d577b634657bc753607
Cameron Frazier <frazier.cameron@gmail.com>
2009-05-10 19:07:58 -0400

    [Wifi] General cleanup, added ability to call wifiman.py (currently commented out)

--- shr_settings_modules/shr_wifi.py
+++ shr_settings_modules/shr_wifi.py
@@ -1,10 +1,4 @@
 import elementary, module, os, dbus
-try:
-    from pythonwifi.iwlibs import Wireless, getNICnames
-    iwlibs_present = 1
-except:
-    print "from pythonwifi.iwlibs import Wireless, getNICnames import error - not present"
-    iwlibs_present = 0
 
 # Locale support
 import gettext
@@ -20,79 +14,152 @@ def getDbusObject (bus, busname , objectpath , interface):
         dbusObject = bus.get_object(busname, objectpath)
         return dbus.Interface(dbusObject, dbus_interface=interface)
 
+
+class WifiManageBox(elementary.Box):
+    """
+    Class for Wifi Manager Button
+    """
+
+    def update(self):
+        """
+        """
+        if self.dbusObj.GetPower():
+            self.show()
+        else:
+            self.hide()
+
+    def buttonClicked(self, obj, event, *args, **kargs):
+        """
+        Trigger running the Wifi manager 'wifiman.py'
+
+        !!!THIS IS ONLY FOR TESTING!!!
+
+        Maybe in the future this will do something; currently it's GNDN.
+        Must be adapted to your personal settings
+        """
+##        print "Running wifiman.py"
+##        os.system("/home/root/Programs/wifiman.py")
+        pass
+
+    def __init__(self, win, dbusObj):
+        """
+        initialize the box and load objects
+        Update the toggles to match current system settings
+        """
+
+        super(WifiManageBox, self).__init__(win)
+        self.window = win
+        self.dbusObj = dbusObj
+
+        self.button = elementary.Button(self.window)
+##        self.button.label_set(_("WiFi Manager"))
+        self.button.label_set(_("WiFi Manager")+" (GNDN)")
+        self.button.clicked = self.buttonClicked
+        self.button.size_hint_align_set(-1.0, 0.0)
+        self.button.show()
+
+        self.pack_start(self.button)
+        self.size_hint_align_set(-1.0, 0.0)
+
+        self.update()
+        self.show()
+
+
+class WifiToggleBox(elementary.Box):
+    """
+    Class for Wifi Connection power toggle
+    """
+
+    def update(self):
+        """
+        Set the toggle to the wifi power state
+        """
+        self.state = self.dbusObj.GetPower()
+        self.toggle.state_set(self.state)
+
+    def toggleChanged(self, obj, event, *args, **kargs):
+        """
+        Toggle the resource to opposite the toggle changed-to state
+        Update the toggles to match current system settings
+        """
+        self.dbusObj.SetPower(obj.state_get())
+        #self.update()
+
+    def __init__(self, win, dbusObj):
+        """
+        initialize the box and load objects
+        Update the toggles to match current system settings
+        """
+
+        super(WifiToggleBox, self).__init__(win)
+        self.window = win
+        self.dbusObj = dbusObj
+        self.state = None
+
+        self.toggle = elementary.Toggle(self.window)
+        self.toggle.label_set(_("WiFi radio:"))
+        self.toggle.states_labels_set(_("On"),_("Off"))
+        self.toggle.changed = self.toggleChanged
+        self.toggle.size_hint_align_set(-1.0, 0.0)
+        self.toggle.show()
+
+        self.pack_start(self.toggle)
+        self.size_hint_align_set(-1.0, 0.0)
+
+        self.update()
+        self.show()
+
+
 class Wifi(module.AbstractModule):
     name = _("WiFi")
+
+    def error(self):
+        label = elementary.Label(self.window)
+        label.label_set("Dbus is borked")
+        label.show()
+        self.main.pack_start(label)
 
     def isEnabled(self):
         if os.popen("cat /proc/cpuinfo | grep Hardware |  awk '{ print $3 }'").read()=="GTA01\n":
             return False
         else:
             return True
-    
-    def scan_wifi(self):
 
-        found_aps = {}
-
-        # search for accesspoints the interfaces can access..
-        # and show list of connections to user..
-        for ifname in getNICnames():
-
-            wifi = Wireless(ifname)
-            essid = wifi.getEssid()
-
-            wifi.scan()
-            for results in wifi.scan():
-                enc_type = "unknown"
-                # seems encryption [0x0,0x0,0x0,0x8] is WPA, [0x0,0x0,0x0,0x80] is open
-                #TODO - detect WEP encryption
-                enc = map(lambda x: hex(ord(x)), results.encode)
-
-                if enc[3] == '0x80':
-                    enc_type = "open"
-                elif  enc[3] == '0x8':
-                    enc_type = "WPA"
-
-                found_aps[results.essid] = {'essid':results.essid, 'enc':enc_type, 'connected': (essid == results.essid)}
-        return found_aps
-
-    def power_handle(self, obj, event, *args, **kargs):
-	if self.wifi.GetPower()==obj.state_get():
-		return 0
-	self.wifi.SetPower(obj.state_get())
+    def update(self, *args, **kargs):
+        """
+        """
+        self.wifiToggle.update()
+        self.wifiManage.update()
 
     def createView(self):
+        self.main = elementary.Box(self.window)
+
         try:
-                self.wifi = getDbusObject (self.dbus, "org.freesmartphone.odeviced", "/org/freesmartphone/Device/PowerControl/WiFi", "org.freesmartphone.Device.PowerControl")
-        except:
-            label = elementary.Label(self.window)
-            label.label_set(_("can't connect to dbus"))
-            return label
 
-        box1 = elementary.Box(self.window)
+            if self.isEnabled():
+                # connect to dbus
+                self.dbusObj = getDbusObject (self.dbus,
+                    "org.freesmartphone.odeviced",
+                    "/org/freesmartphone/Device/PowerControl/WiFi",
+                    "org.freesmartphone.Device.PowerControl")
 
-        toggle0 = elementary.Toggle(self.window)
-        toggle0.label_set(_("WiFi radio:"))
-        toggle0.size_hint_align_set(-1.0, 0.0)
-        toggle0.states_labels_set(_("On"),_("Off"))
-        toggle0.changed = self.power_handle
-        box1.pack_start(toggle0)
-        toggle0.state_set(self.wifi.GetPower())
-        toggle0.show()
-        
-#        try:
-#            networks = self.scan_wifi()
-##            print networks
-#            for net in networks:
-#                btn1 = elementary.Button(self.window)
-#                btn1.label_set(net)
-#                box1.pack_end(btn1)
-#                btn1.show()
-#        except:
-#            label1 = elementary.Label(self.window)
-#            global iwlibs_present
-#            if iwlibs_present == 1:
-#                label1.label_set(_("unable to scan"))
-#                box1.pack_end(label1)
-#                label1.show()
+                # connect to dbus signals
+                self.dbusObj.connect_to_signal("Power", self.update)
 
-        return box1
+                self.wifiToggle = WifiToggleBox(self.window, self.dbusObj)
+##                self.wifiManage = WifiManageBox(self.window, self.dbusObj)
+
+                self.main.pack_start(self.wifiToggle)
+##                self.main.pack_end(self.wifiManage)
+
+                self.update()
+            else:
+                label = elementary.Label(self.window)
+                label.label_set("Wifi not available\non GTA01 Hardware")
+                label.show()
+                self.main.pack_start(label)
+
+        except dbus.exceptions.DBusException, e:
+            self.error()
+
+        return self.main
