5daee2f02e0768ad7d63b6cf2cd63af0d0e8cbec
Cameron Frazier <frazier.cameron@gmail.com>
2009-05-19 19:11:11 -0400

    [Battery] Cleaned up references to /sys/ nodes for easier future changes. [Battery] Added charge rate control [Battery] Restructured the displayed data (Suggestions requested)

--- shr_settings_modules/shr_battery.py
+++ shr_settings_modules/shr_battery.py
@@ -5,6 +5,21 @@ import threading
 # Locale support
 import gettext
 
+
+SYSNODE = {
+    # "short name"  : ("/sys/node/loaction", scaling_factor = 1)
+    "charge_limit"  : ("/sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/chg_curlim", 1),
+    "usb_limit"     : ("/sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/usb_curlim", 1),
+    "capacity"      : ("/sys/class/power_su*ply/bat*/capacity", 1),
+    "current"       : ("/sys/class/power_su*ply/bat*/current_now", 1e-3),
+    "temperature"   : ("/sys/class/power_sup*ly/bat*/temp", 1e-1),
+    "voltage"       : ("/sys/class/power_sup*ly/bat*/voltage_now", 1e-6),
+    "status"        : ("/sys/class/power_su*ply/bat*/status", 1),
+    "time_to_full"  : ("/sys/class/power_su*ply/bat*/time_to_full_now", 1),
+    "time_to_empty" : ("/sys/class/power_su*ply/bat*/time_to_empty_now", 1),
+    }
+
+
 try:
     cat = gettext.Catalog("shr-settings")
     _ = cat.gettext
@@ -39,6 +54,59 @@ class BatteryLabel(elementary.Label):
         self.label_set(finalLabel)
 
 
+class FastChargeBox(elementary.Box):
+    """
+    Class for fast charge toggle
+    """
+
+    def update(self):
+        """
+        Set toggle visibility
+        """
+
+        value   = os.popen("cat "+self.chgNodePath).readline().strip()
+
+        if int(value) >= 500:
+            self.toggle.state_set(True)
+        else:
+            self.toggle.state_set(False)
+
+    def toggleChanged(self, obj, event, *args, **kargs):
+        """
+        Toggle the charge rate on/off
+        """
+        if obj.state_get():
+            os.popen("echo 500 > "+self.usbNodePath)
+        else:
+            os.popen("echo 100 > "+self.usbNodePath)
+        self.update()
+
+    def __init__(self, win):
+        """
+        initialize the box and load objects
+        Update the toggles to match current system settings
+        """
+
+        super(FastChargeBox, self).__init__(win)
+        self.window = win
+
+        self.chgNodePath    = SYSNODE["charge_limit"][0]
+        self.usbNodePath    = SYSNODE["usb_limit"][0]
+
+        self.toggle = elementary.Toggle(self.window)
+        self.toggle.label_set("USB Charging Rate")
+        self.toggle.states_labels_set("500 mA","100 mA")
+        self.toggle.changed = self.toggleChanged
+        self.toggle.size_hint_align_set(-1.0, 0.0)
+        self.toggle.show()
+
+        self.pack_start(self.toggle)
+        self.size_hint_align_set(-1.0, 0.0)
+
+        self.update()
+        self.show()
+
+
 class Battery(module.AbstractModule):
     name = _("Battery settings")
 
@@ -51,7 +119,7 @@ class Battery(module.AbstractModule):
         self.main.pack_start(label)
         label.show()
 
-    def getValue(self,nodePath, scale = 1):
+    def getValue(self,nodePath):
         """
         Given a /sys node path and scaling factor,
         return the intelligent result (int, float, str)
@@ -60,7 +128,8 @@ class Battery(module.AbstractModule):
         but is implicit with int values
         """
         # retrieve raw value
-        value = os.popen("cat "+nodePath).readline().strip()
+        value = os.popen("cat "+SYSNODE[nodePath][0]).readline().strip()
+        scale = SYSNODE[nodePath][1]
 
         # try some intelligent filtering
         try:
@@ -85,7 +154,7 @@ class Battery(module.AbstractModule):
         min     = int( ( sec % 3600 ) / 60 )
         return ( hours, min )
 
-    def refreshAct(self, signalData = None):
+    def update(self, signalData = None):
         """
         Update the data displayed in BatteryLabel
         """
@@ -93,32 +162,26 @@ class Battery(module.AbstractModule):
         try:
 
             # read data from /sys nodes
-            cap     = self.getValue("/sys/class/power_su*ply/bat*/capacity")
-            cur     = self.getValue("/sys/class/power_su*ply/bat*/current_now", 1e-3)
-            temp    = self.getValue("/sys/class/power_sup*ly/bat*/temp", 1e-1)
-            vol     = self.getValue("/sys/class/power_sup*ly/bat*/voltage_now", 1e-6)
-            sta     = self.getValue("/sys/class/power_su*ply/bat*/status")
+            cap     = self.getValue("capacity")
+            cur     = self.getValue("current")
+            temp    = self.getValue("temperature")
+            vol     = self.getValue("voltage")
+            sta     = self.getValue("status")
             if sta == "Charging":
-                time = self.getValue("/sys/class/power_su*ply/bat*/time_to_full_now")
-                try:
-                    rate = self.getValue("/sys/class/i2c-adapter/i2c-0/0-0073/pcf50633-mbc/chg_curlim")
-                    rate = " ({0} mA)".format(rate)
-                    sta += rate
-                except:
-                    pass
+                time = self.getValue("time_to_full")
             else:
-                time = self.getValue("/sys/class/power_su*ply/bat*/time_to_empty_now")
+                time = self.getValue("time_to_empty")
 
             # calculate hours and minutes from the found time value
             time = self.sectotime(time)
 
             # set the displayed labels
-            self.timel.setLabel(time)
-            self.capl.setLabel(cap)
-            self.curl.setLabel(cur)
-            self.templ.setLabel(temp)
-            self.voll.setLabel(vol)
-            self.stal.setLabel(sta)
+            #
+            self.charge.setLabel( (cap, time) )
+            self.power.setLabel(  (cur, vol) )
+            self.status.setLabel( (sta, temp) )
+
+            self.fastChargeToggle.update()
 
         except:
             return 1
@@ -140,27 +203,35 @@ class Battery(module.AbstractModule):
                 "org.freesmartphone.Device.PowerSupply")
 
             # set update triggers
-            self.dbusObj.connect_to_signal("Capacity",      self.refreshAct)
-            self.dbusObj.connect_to_signal("PowerStatus",   self.refreshAct)
+            self.dbusObj.connect_to_signal("Capacity",      self.update)
+            self.dbusObj.connect_to_signal("PowerStatus",   self.update)
 
             # create labels, setting the display formats and static labels
-            self.stal   = BatteryLabel(self.window, "{0}{1}",                       _("Status: "))
-            self.voll   = BatteryLabel(self.window, "{0}{1:.3f} V",                 _("Voltage: "))
-            self.templ  = BatteryLabel(self.window, "{0}{1:.1f} 'C",                _("Temperature: "))
-            self.curl   = BatteryLabel(self.window, "{0}{1:.0f} mA",                _("Current: "))
-            self.capl   = BatteryLabel(self.window, "{0}{1} %",                     _("Capacity: "))
-            self.timel  = BatteryLabel(self.window, "{0}{1[0]:d} h {1[1]:02d} min", _("Remaining time: "))
+
+            # label display formats
+            #
+            #   !!! This needs improvement !!!
+            #   !!! Anyone have suggestions on presentation?
+            #
+            chargeFormat    = "{0}{1[0]}%, ({1[1][0]:d}:{1[1][1]:02d})"
+            powerFormat     = "{0}{1[0]:.0f} mA; {1[1]:.3f} V"
+            statusFormat    = "{0}{1[0]} @ {1[1]:.1f} 'C"
+
+            self.charge = BatteryLabel(self.window, chargeFormat,   _("Charge: "))
+            self.power  = BatteryLabel(self.window, powerFormat,    _("Power Info: "))
+            self.status = BatteryLabel(self.window, statusFormat,   _("Status: "))
+
+            self.fastChargeToggle = FastChargeBox(self.window)
 
             # pack labels into the box
-            self.main.pack_start(self.stal)
-            self.main.pack_start(self.voll)
-            self.main.pack_start(self.templ)
-            self.main.pack_start(self.curl)
-            self.main.pack_start(self.capl)
-            self.main.pack_start(self.timel)
+            self.main.pack_end(self.charge)
+            self.main.pack_end(self.power)
+            self.main.pack_end(self.status)
+
+            self.main.pack_end(self.fastChargeToggle)
 
             # update the labels
-            self.refreshAct()
+            self.update()
 
             self.main.show()
 
@@ -170,4 +241,3 @@ class Battery(module.AbstractModule):
             self.error()
 
         return self.main
-
