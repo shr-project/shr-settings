e2e14ca05cbdb5e792efc79882f7d56a58490e01
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-06-17 00:26:13 +0200

    [date] increase size of font a bit

--- shr_settings_modules/shr_date.py
+++ shr_settings_modules/shr_date.py
@@ -17,4 +17,5 @@ class Date(module.AbstractModule):
     def createView(self):
         la = elementary.Label(self.window)
         la.label_set(date.today().strftime("%A, %d %B %Y"))
+        la.scale_set(1.5)
         return la
