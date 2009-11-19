b92390148cf0e979fd4c678a6633bb5e436d5763
Sebastian Spaeth <Sebastian@SSpaeth.de>
2009-11-19 14:46:28 +0100

    shr_wifi: make Wifi toggling work with new DBus paths
    * Removed experimental Wifiman code. Pull it out of the archive if needed.
    * Use fsousaged to toggle Wifi not odeviced (whose dbus paths have changed, so this was not working anymore anyway)
    * make initial toggle display async
    * This makes the module load around 0.5 seconds faster
    
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- shr_settings_modules/shr_wifi.py
+++ shr_settings_modules/shr_wifi.py
@@ -14,100 +14,67 @@ def getDbusObject (bus, busname , objectpath , interface):
         dbusObject = bus.get_object(busname, objectpath)
         return dbus.Interface(dbusObject, dbus_interface=interface)
 
-
-class WifiManageBox(elementary.Box):
-    """
-    Class for Wifi Manager Button
-    """
-
-    def doNothing(self, obj, event, *args, **kargs):
-        print "Nothing Done"
-
-    def update(self):
-        """
-        """
-        if self.dbusObj.GetPower():
-            self.button.label_set(_("WiFi Manager"))
-            self.button.clicked = self.buttonClicked
-        else:
-            self.button.label_set(_("WiFi Manager")+" (Off)")
-            self.button.clicked = self.doNothing
-
-    def buttonClicked(self, obj, event, *args, **kargs):
-        """
-        Trigger running the Wifi manager 'wifiman.py'
-
-        !!!THIS IS ONLY FOR TESTING!!!
-
-        Maybe in the future this will do something; currently it's GNDN.
-        Must be adapted to your personal settings
-        """
-        print "Running wifiman.py"
-        os.system("/home/root/Programs/wifiman.py")
-
-    def __init__(self, win, dbusObj):
-        """
-        initialize the box and load objects
-        Update the toggles to match current system settings
-        """
-
-        super(WifiManageBox, self).__init__(win)
-        self.window = win
-        self.dbusObj = dbusObj
-
-        self.button = elementary.Button(self.window)
-        self.button.size_hint_align_set(-1.0, 0.0)
-        self.button.show()
-
-        self.pack_start(self.button)
-        self.size_hint_align_set(-1.0, 0.0)
-
-        self.update()
-        self.show()
-
-
+ 
 class WifiToggleBox(elementary.Box):
-    """
-    Class for Wifi Connection power toggle
-    """
+    """ Class for Wifi Connection power toggle """
 
-    def update(self):
-        """
-        Set the toggle to the wifi power state
-        """
-        self.state = self.dbusObj.GetPower()
-        self.toggle.state_set(self.state)
+    def update(self, state):
+        """ Set the toggle to the wifi power state """
+        self.toggle.state_set(state)
 
     def toggleChanged(self, obj, event, *args, **kargs):
         """
         Toggle the resource to opposite the toggle changed-to state
         Update the toggles to match current system settings
         """
-        self.dbusObj.SetPower(obj.state_get())
-        self.update()
+        policy = 'disabled'
+        if obj.state_get(): policy = 'enabled'
+        self.dbusObj.SetResourcePolicy('WiFi', policy)
 
-    def __init__(self, win, dbusObj):
+    def init_toggle_reply_handler(self, state):
+        """ Handler that sets the initial toggle setting 
+            It also makes the toggle visible
+        """
+        self.update(state)
+        self.pack_start(self.toggle)
+        self.toggle.show()
+
+    def init_toggle_error_handler(self, e):
+        """ if the initial state could not be retrieved """
+        # we'll not show the toggle here
+        print "received exception " + str(e)
+        reason = 'Unknown DBus Exception'
+        if e._dbus_error_name == 'org.freesmartphone.Usage.ResourceUnknown':
+            reason= "Wifi not available"
+        # Show an error message instead
+        label = elementary.Label(self.wifi.main)
+        label.label_set(reason)
+        label.show()
+        self.wifi.main.pack_start(label)
+
+    def __init__(self, wifi, dbusObj):
         """
         initialize the box and load objects
         Update the toggles to match current system settings
         """
 
-        super(WifiToggleBox, self).__init__(win)
-        self.window = win
+        super(WifiToggleBox, self).__init__(wifi.window)
+        self.wifi = wifi
         self.dbusObj = dbusObj
         self.state = None
 
-        self.toggle = elementary.Toggle(self.window)
+        self.toggle = elementary.Toggle(self.wifi.window)
         self.toggle.label_set(_("WiFi radio:"))
         self.toggle.states_labels_set(_("On"),_("Off"))
         self.toggle.changed = self.toggleChanged
         self.toggle.size_hint_align_set(-1.0, 0.0)
-        self.toggle.show()
 
-        self.pack_start(self.toggle)
+        #ask async for WiFi state and set & show toggle then
+        self.dbusObj.GetResourceState('WiFi', 
+          reply_handler = self.init_toggle_reply_handler,
+          error_handler = self.init_toggle_error_handler)
+
         self.size_hint_align_set(-1.0, 0.0)
-
-        self.update()
         self.show()
 
 
@@ -120,61 +87,29 @@ class Wifi(module.AbstractModule):
         label.show()
         self.main.pack_start(label)
 
-    def isEnabled(self):
-        sysCmd = "cat /proc/cpuinfo | grep Hardware |  awk '{ print $3 }'"
-        if os.popen(sysCmd).read()=="GTA01\n":
-            return False
-        else:
-            return True
-
-    def wifiManageCheck(self):
-        if self.wifiToggle.state:
-            try:
-                self.wifiManage.update()
-            except:
-                self.wifiManage = WifiManageBox(self.window, self.dbusObj)
-                self.main.pack_end(self.wifiManage)
-                self.wifiManage.update()
-        else:
-            try:
-                self.wifiManage.delete()
-                del self.wifiManage
-            except:
-                pass
-
-    def update(self, *args, **kargs):
+    def update(self, name, state, attr):
+        """ catches the ResourceChanged signal and acts if WiFi changes
         """
-        """
-        self.wifiToggle.update()
-##        self.wifiManageCheck()
+        if name != 'WiFi': return
+        self.wifiToggle.update(state)
 
     def stopUpdate(self):
         self.signal.remove()
 
     def createView(self):
         self.main = elementary.Box(self.window)
-
         try:
-            if self.isEnabled():
-                # connect to dbus
-                self.dbusObj = getDbusObject (self.dbus,
-                    "org.freesmartphone.odeviced",
-                    "/org/freesmartphone/Device/PowerControl/WiFi",
-                    "org.freesmartphone.Device.PowerControl")
+            # connect to dbus
+            self.dbusObj = getDbusObject (self.dbus,
+                                          "org.freesmartphone.ousaged",
+                                          "/org/freesmartphone/Usage",
+                                          "org.freesmartphone.Usage")
 
-                # connect to dbus signals
-                self.signal = self.dbusObj.connect_to_signal("Power", self.update)
-
-                # create/pack toggle box
-                self.wifiToggle = WifiToggleBox(self.window, self.dbusObj)
-                self.main.pack_start(self.wifiToggle)
-
-                self.update()
-            else:
-                label = elementary.Label(self.window)
-                label.label_set("Wifi not available\non GTA01 Hardware")
-                label.show()
-                self.main.pack_start(label)
+            # connect to dbus signals
+            self.signal = self.dbusObj.connect_to_signal("ResourceChanged", self.update)
+            # create/pack toggle box
+            self.wifiToggle = WifiToggleBox(self, self.dbusObj)
+            self.main.pack_start(self.wifiToggle)
 
         except dbus.exceptions.DBusException, e:
             self.error()
