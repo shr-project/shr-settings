6ef8b3a39f8a20c21e333e23d92824434eba607a
Pau Espin Pedrol <pespin.shar@gmail.com>
2010-09-30 00:07:41 +0200

    [Timezone] changed hoversel for inwin. I couldn't get the List to show, so I implemented it with an array of buttons. Would be good to have it with a List + Index though.

--- shr_settings_modules/shr_timezone.py
+++ shr_settings_modules/shr_timezone.py
@@ -19,68 +19,85 @@ except IOError:
 class Timezone(module.AbstractModule):
     name = _("Timezone settings")
     tzdir = "/usr/share/zoneinfo"
-    try:
-        tzfile = open("/etc/timezone", "w+")
-    except:
-        print "Error opening /etc/timezone"
-     
-    def setNewTimezone(self, prefix, timezone):
+    
+    def setNewTimezone(self, timezone):
         try:
             tzfile = open("/etc/timezone", "w+")
         except:
             print "Error opening /etc/timezone"
             return
-        tzfile.writelines(os.path.join(prefix, timezone))
+        tzfile.writelines(timezone)
         tzfile.close()
         if os.path.isfile("/etc/localtime"):
             print "removing old /etc/localtime"
             os.system("rm /etc/localtime")
-        ret = os.system("ln  -s %s /etc/localtime" % os.path.join(self.tzdir,prefix,timezone))
+        ret = os.system("ln  -s %s /etc/localtime" % os.path.join(self.tzdir,timezone))
         if ret:
             print "error linking timezone to /etc/localtime"
-                
-    def receivedTimezone0(self, timezone, obj, *args, **kargs):
-        print timezone
-        if self.hs1exists==1:
-            self.hoverSel1.delete()
-        self.hoverSel0.label_set(timezone)
-        if os.path.isdir(os.path.join(self.tzdir, timezone)):
-            self.createhoverSel1(timezone)
-        else:
-            self.setNewTimezone("", timezone)
             
-    def receivedTimezone1(self, prefix, timezone, obj, *args, **kargs):
-        print os.path.join(prefix, timezone)
-        self.hoverSel1.label_set(timezone)
-        if os.path.isfile(os.path.join(self.tzdir, prefix, timezone)):
-            self.setNewTimezone(prefix, timezone)
-        
-    def createhoverSel0(self):
-        self.hs1exists=0
-        self.hoverSel0 = elementary.Hoversel(self.window)
-        self.hoverSel0.hover_parent_set(self.window)
-        self.hoverSel0.size_hint_weight_set(-1.0, 0.0)
-        self.hoverSel0.size_hint_align_set(-1.0, 0.0)
-        self.main.pack_end(self.hoverSel0)
-        self.hoverSel0.show()
-        for i in os.listdir(self.tzdir):
-            self.hoverSel0.item_add(i, "arrow_down", elementary.ELM_ICON_STANDARD, partial( self.receivedTimezone0, i))
-        return self.hoverSel0
-        
-    def createhoverSel1(self, subdir):
-        self.hs1exists = 1
-        self.hoverSel1 = elementary.Hoversel(self.window)
-        self.hoverSel1.hover_parent_set(self.window)
-        self.hoverSel1.size_hint_weight_set(-1.0, 0.0)
-        self.hoverSel1.size_hint_align_set(-1.0, 0.0)
-        self.main.pack_end(self.hoverSel1)
-        self.hoverSel1.show()
-        for i in os.listdir(os.path.join(self.tzdir,subdir)):
-            self.hoverSel1.item_add(i, "arrow_down", elementary.ELM_ICON_STANDARD, partial ( self.receivedTimezone1, subdir, i))
-        return self.hoverSel1
+            
+    def receivedTimezone(self, timezone, obj, *args, **kargs):
+        self.setNewTimezone(timezone)
+        self.closeTzList()
     
+    def generateTimezones(self, prefix):
+        
+        tzs = [ os.path.join(prefix,f) for f in os.listdir(os.path.join(self.tzdir,prefix)) if os.path.isfile(os.path.join(self.tzdir,prefix,f)) ]
+        for i in os.listdir(os.path.join(self.tzdir,prefix)):
+            if os.path.isdir(os.path.join(self.tzdir,prefix,i)):
+                subtzs = self.generateTimezones(os.path.join(prefix, i))
+                for f in subtzs:
+                    tzs.append(f)
+        return tzs
+    
+    def populateTzList(self):
+        self.timezones = self.generateTimezones("")
+        self.timezones = sorted(self.timezones)
+        for tz in self.timezones:
+            btz = elementary.Button(self.inwin)
+            btz.label_set(tz)
+            btz.size_hint_weight_set(-1.0, 0.0)
+            btz.size_hint_align_set(-1.0, 0.0)
+            self.inbox.pack_end(btz)
+            btz.show()
+            btz._callback_add('clicked', partial(self.receivedTimezone, tz))
+        
+    
+    def createTzList(self, *args):
+        
+        self.inwin = elementary.InnerWindow(self.window)
+        self.inwin.style_set('default')
+        self.window.resize_object_add(self.inwin)
+        self.inwin.show()
+        self.inwin.activate()
+        
+        self.scr = elementary.Scroller(self.inwin)
+        self.scr.bounce_set(0,0)
+        self.scr.size_hint_weight_set(1.0, 1.0)
+        self.scr.size_hint_align_set(-1.0, -1.0)
+        self.inwin.content_set(self.scr)
+        self.scr.show()
+        
+        self.inbox = elementary.Box(self.inwin)
+        self.scr.content_set(self.inbox)
+        
+        self.populateTzList()
+ 
+        
+    def closeTzList(self, *args):
+        self.inwin.delete()
+  
+
+    def createButton(self):
+        self.bt = elementary.Button(self.window)
+        self.bt.label_set(_("Set Timezone"))
+        self.bt.size_hint_weight_set(-1.0, 0.0)
+        self.bt.size_hint_align_set(-1.0, 0.0)
+        self.main.pack_end(self.bt)
+        self.bt.show()
+        self.bt._callback_add('clicked', self.createTzList)
+        
     def createView(self):
         self.main = elementary.Box(self.window)
-        
-        self.createhoverSel0()
+        self.createButton()
         return self.main
